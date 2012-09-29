4c57a5ef65c63c25c4166320d59fa7a83f889f1c
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2012-09-29 17:43:37 +0100

    Basic syllable count.

--- accidental_haiku.rb
+++ accidental_haiku.rb
@@ -1,9 +1,48 @@
 class AccidentalHaiku
   include Cinch::Plugin
 
-  match 'hello'
+  match /count (.*)/
 
-  def execute(message)
-    message.reply "Hello, #{message.user.nick}"
+  def execute(message, string)
+    message.reply "A total of #{syllable_count(string)} syllables."
   end
+
+protected
+
+  def syllable_count(string)
+    # Original written by Elisabeth Hendrickson, Quality Tree Software, Inc.
+    # Copyright (c) 2009 Quality Tree Software, Inc.
+    #
+    # Edits by Elliot Crosby-McCullough
+    #
+    # This work is licensed under the
+    # Creative Commons Attribution 3.0 United States License.
+    #
+    # To view a copy of this license, visit
+    #      http://creativecommons.org/licenses/by/3.0/us/
+    consonants = "bcdfghjklmnpqrstvwxz"
+    vowels = "aeiouy"
+    processed = string.downcase
+    suffix_bonus = 0
+
+    if processed.match(/ly$/)
+      suffix_bonus = 1
+      processed.gsub!(/ly$/, "")
+    end
+
+    if processed.match(/[a-z]ed$/)
+      # Not counting "ed" as an extra symbol.
+      # So 'blessed' is assumed to be said as 'blest'
+      suffix_bonus = 0
+      processed.gsub!(/ed$/, "")
+    end
+
+    processed.gsub!(/iou|eau|ai|au|ay|ea|ee|ei|oa|oi|oo|ou|ui|oy/, "@") #vowel combos
+    processed.gsub!(/qu|ng|ch|rt|[#{consonants}h]/, "=") #consonant combos
+    processed.gsub!(/[#{vowels}@][#{consonants}=]e$/, "@|") # remove silent e
+    processed.gsub!(/[#{vowels}]/, "@") #all remaining vowels will be counted
+
+    return processed.count("@") + suffix_bonus
+  end
+
 end
